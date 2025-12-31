b2b5d54302cdb47fc80a63d6bd8a05affec8f681
jjzn <50965831+jjzn@users.noreply.github.com>
2025-12-31 22:52:29 +0100

    Serve login/overview page

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@
 
 use chrono::{Duration, Utc};
 use rocket::form::Form;
+use rocket::fs::NamedFile;
 use rocket::http::{Cookie, CookieJar, SameSite, Status};
 use rocket::request::{Request, FromRequest, Outcome};
 use rocket::response::Redirect;
+use rocket::serde::Serialize;
 use rocket_db_pools::{Connection, Database, sqlx};
 use rocket_db_pools::sqlx::Row;
+use rocket_dyn_templates::{Template, context};
 
 #[derive(Database)]
 #[database("appdata")]
@@ -18,6 +21,8 @@ struct LoginData {
     password: String
 }
 
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
 struct Player {
     id: String,
     email: String,
@@ -31,25 +36,27 @@ impl<'r> FromRequest<'r> for Player {
     async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
         let session_id = match req.cookies().get("sessionId") {
             Some(cookie) => cookie.value(),
-            None => return Outcome::Error((Status::Unauthorized, ()))
+            None => return Outcome::Forward(Status::Unauthorized)
         };
 
         let mut db = match req.guard::<Connection<AppData>>().await {
             Outcome::Success(db) => db,
+            // We use Outcome::Error instead of Outcome::Forward because we want
+            // the user to know that a server error ocurred
             _ => return Outcome::Error((Status::InternalServerError, ()))
         };
 
         let row = match sqlx::query("SELECT playerId, expires FROM sessions WHERE id = $1").bind(session_id)
             .fetch_one(&mut **db).await {
             Ok(row) => row,
-            Err(_) => return Outcome::Error((Status::Unauthorized, ()))
+            Err(_) => return Outcome::Forward(Status::Unauthorized)
         };
 
         let player_id: String = row.get(0);
         let expiration: i64 = row.get(1);
 
         if Utc::now().timestamp() >= expiration {
-            return Outcome::Error((Status::Unauthorized, ()));
+            return Outcome::Forward(Status::Unauthorized);
         }
 
         let row = match sqlx::query("SELECT email, name FROM players WHERE id = $1").bind(&player_id)
@@ -63,13 +70,13 @@ impl<'r> FromRequest<'r> for Player {
 }
 
 #[get("/")]
-fn index_auth(player: Player) -> String {
-    format!("Hello, {} with email {}", player.name, player.email)
+fn index_auth(player: Player) -> Template {
+    Template::render("index", context! { player })
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/", rank = 2)]
+async fn index() -> Option<NamedFile> {
+    NamedFile::open("public/index.html").await.ok()
 }
 
 #[post("/login", data = "<form>")]
@@ -104,5 +111,6 @@ async fn login(form: Form<LoginData>, cookies: &CookieJar<'_>, mut db: Connectio
 fn rocket() -> _ {
     rocket::build()
         .attach(AppData::init())
+        .attach(Template::fairing())
         .mount("/", routes![index, index_auth, login])
 }
