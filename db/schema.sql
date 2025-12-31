85c711aefe65525103cb6e6c550bcacafc2cfa4a
jjzn <50965831+jjzn@users.noreply.github.com>
2025-12-31 18:29:00 +0100

    Write schema definition

---
+++ db/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE players (
+	id TEXT PRIMARY KEY,
+	email TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL,
+	name TEXT NOT NULL UNIQUE
+) STRICT;
+
+CREATE TABLE tournaments (
+	name TEXT NOT NULL,
+	year INTEGER NOT NULL,
+	PRIMARY KEY (name, year)
+) STRICT;
+
+CREATE TABLE teams (
+	id TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	captainId TEXT NOT NULL,
+	partnerId TEXT NOT NULL,
+	tournamentName TEXT NOT NULL,
+	tournamentYear INTEGER NOT NULL,
+	FOREIGN KEY (captainId) REFERENCES players(id),
+	FOREIGN KEY (partnerId) REFERENCES players(id),
+	FOREIGN KEY (tournamentName, tournamentYear) REFERENCES tournaments(name, year)
+) STRICT;
+
+CREATE TABLE game (
+	date INTEGER NOT NULL,
+	scoresA INTEGER NOT NULL,
+	scoresB INTEGER NOT NULL,
+	acceptedByA INTEGER,
+	acceptedByB INTEGER,
+	teamA TEXT NOT NULL,
+	teamB TEXT NOT NULL,
+	FOREIGN KEY (teamA) REFERENCES teams(id),
+	FOREIGN KEY (teamB) REFERENCES teams(id)
+) STRICT;
