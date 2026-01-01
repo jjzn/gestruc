0af977fe17ee9542e65c7621549b80573f58818f
jjzn <50965831+jjzn@users.noreply.github.com>
2026-01-01 20:02:40 +0100

    Add mock data script

---
+++ db/populate_mock.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys = ON;
+
+INSERT INTO players VALUES('user01','jjzn@protonmail.com','test123','Joshua Zien');
+INSERT INTO players VALUES('user02','toni@fuentes.com','test123','Toni Fuentes');
+INSERT INTO players VALUES('user03','03@usuaris.com','test123','Usuari 03');
+INSERT INTO players VALUES('user04','04@usuaris.com','test123','Usuari 04');
+
+INSERT INTO tournaments VALUES('Lliga UIB',2025);
+
+INSERT INTO teams VALUES('team01','Envit de Fenwick','user02','user01','Lliga UIB',2025);
+INSERT INTO teams VALUES('team02','Retrucs','user03','user04','Lliga UIB',2025);
+
+INSERT INTO games VALUES('game01',1767294000,0,0,NULL,1767300300,'team01','team02');
