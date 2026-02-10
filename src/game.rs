jjzn/gestruc

commit 145567856941c84e2a00135cd1dcf38e10c03db3

Move module definitions into lib.rs
This allows definitions to be reused in other targets, such as
standalone CLI tooling.

src/game.rs
@@ -1,11 +1,11 @@
 use chrono::{DateTime, NaiveDateTime, Utc};
 use chrono_tz::Europe;
 use rocket::{form::Form, serde::Serialize};
-use rocket_db_pools::{Connection, sqlx::{self, Row}};
+use rocket_db_pools::{Connection, sqlx::{self, Execute, Row}};
 
 use crate::{AppData, error::AppError, team::Team};
 
-#[derive(FromForm)]
+#[derive(rocket::FromForm)]
 #[allow(non_snake_case)]
 pub struct GameData {
     pub opponentName: String,
@@ -28,9 +28,15 @@ impl From<u32> for GameScore {
     }
 }
 
+impl From<&GameScore> for u32 {
+    fn from(score: &GameScore) -> Self {
+        (score.0 as u32) | ((score.1 as u32) << 8) | ((score.2 as u32) << 16)
+    }
+}
+
 impl From<GameScore> for u32 {
     fn from(score: GameScore) -> Self {
-        (score.0 as u32) | ((score.1 as u32) << 8) | ((score.2 as u32) << 16)
+        score.into()
     }
 }
 

src/lib.rs
@@ -1,0 +1,10 @@
+pub mod error;
+pub mod game;
+pub mod player;
+pub mod team;
+
+use rocket_db_pools::{Database, sqlx};
+
+#[derive(Database)]
+#[database("appdata")]
+pub struct AppData(sqlx::SqlitePool);

src/main.rs
@@ -15,19 +15,11 @@ use rocket_db_pools::{Connection, Database, sqlx};
 use rocket_db_pools::sqlx::Row;
 use rocket_dyn_templates::{Template, context};
 
-mod game;
-mod team;
-mod player;
-mod error;
-
-use crate::error::AppError;
-use crate::game::{Game, GameData};
-use crate::team::{Team};
-use crate::player::Player;
-
-#[derive(Database)]
-#[database("appdata")]
-struct AppData(sqlx::SqlitePool);
+use gestruc::error::AppError;
+use gestruc::game::{Game, GameData};
+use gestruc::team::{Team};
+use gestruc::player::Player;
+use gestruc::AppData;
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]

src/player.rs
@@ -35,7 +35,7 @@ impl Player {
     }
 }
 
-#[async_trait]
+#[rocket::async_trait]
 impl<'r> FromRequest<'r> for Player {
     // We do not need to display detailed app errors, we only care about the HTTP status
     type Error = ();

commit ef690b5c861e84964184d96cdbc8665987ee99e6

Fix infinite recursion introduced in previous commit
Bug was introduced in commit 145567856941c84e2a00135cd1dcf38e10c03db3

src/game.rs
@@ -36,7 +36,7 @@ impl From<&GameScore> for u32 {
 
 impl From<GameScore> for u32 {
     fn from(score: GameScore) -> Self {
-        score.into()
+        (&score).into() // Use the other From<> impl (From<&GameScore>)
     }
 }
 