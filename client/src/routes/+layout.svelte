b0fe4c3b60001f60756ce1d6b4b7d515d5b71fb0
jjzn <50965831+jjzn@users.noreply.github.com>
2026-03-17 23:45:55 +0100

    Initial commit for Svelte frontend

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<nav class="p-4 flex justify-between bg-amber-200">
+    <a href="/">Gestruc</a>
+    <a href="/tournaments">Tornejos</a>
+    <a href="/teams">Equips</a>
+    <a href="/games">Partides</a>
+
+    <div class="flex gap-4">
+        <p>Usuari</p>
+        <a href="/account">account</a>
+    </div>
+</nav>
+
+<main class="p-8">
+    {@render children()}
+</main>
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    const games = ["hola", "mon"];
+</script>
+
+<h1 class="text-3xl">Partides més recents</h1>
+
+<ul>
+    {#each games as game}
+        <li>{game}</li>
+    {/each}
+</ul>
